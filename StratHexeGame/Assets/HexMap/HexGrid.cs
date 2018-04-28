08c01b33c72de0c81a8da530453d40d8bfb8d03b
seb corfa <seb corfa>
2018-04-28 13:07:41 -0400

    Hex map first part

---
+++ StratHexeGame/Assets/HexMap/HexGrid.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    [SerializeField]
+    int Width = 6;
+
+    [SerializeField]
+    int Height = 6;
+
+    [SerializeField]
+    HexCell CellPrefab;
+
+    [SerializeField]
+    Text cellLabelPrefab;
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
+    private HexCell[] cells;
+
+    private Canvas gridCanvas;
+
+    private HexMesh hexMesh;
+
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        cells = new HexCell[Height * Width];
+
+        for (int z = 0, i = 0; z < Height; z++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetric.INNERRADIUS * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetric.INNERRADIUS * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(CellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * Width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+    }
+}
---
+++ StratHexeGame/Assets/HexMap/HexMetric.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class HexMetric {
+    public const float OUTERRADIUS = 10f;
+
+    public const float INNERRADIUS = OUTERRADIUS * 0.866025404f;
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, OUTERRADIUS),
+        new Vector3(INNERRADIUS, 0f, 0.5f * OUTERRADIUS),
+        new Vector3(INNERRADIUS, 0f, -0.5f * OUTERRADIUS),
+        new Vector3(0f, 0f, -OUTERRADIUS),
+        new Vector3(-INNERRADIUS, 0f, -0.5f * OUTERRADIUS),
+        new Vector3(-INNERRADIUS, 0f, 0.5f * OUTERRADIUS),
+        new Vector3(0f, 0f, OUTERRADIUS)
+    };
+}
